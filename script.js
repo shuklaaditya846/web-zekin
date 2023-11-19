b262061db531ed81ce2d29c2ac7e4b7bbf975793
Aditya Shukla <shuklaaditya846@gmail.com>
2023-11-19 19:44:27 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+
+function showImg(elem){
+    // console.log("aaya!");
+    const imagePopup = elem.querySelector('.image-popup');
+    imagePopup.classList.toggle(`image-visible`);
+}
+
+function hideImg(elem){
+    // console.log("gaya!");
+    const imagePopup = elem.querySelector('.image-popup');
+    imagePopup.classList.toggle(`image-visible`);
+}
